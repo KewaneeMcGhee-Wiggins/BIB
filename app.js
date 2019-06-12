f01ce2912909b297822b4d2f727d82fad3bbe445
KewaneeMcGhee-Wiggins <47463588+KewaneeMcGhee-Wiggins@users.noreply.github.com>
2019-06-12 01:38:27 -0400

    views and partials folder w/header and footer...added css styles and color to about.html

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => res.send('Welcome to my app'));//Homepage route
+app.get('/', (req, res) => res.render('index.ejs'));//Homepage route
 // or (req, res) => res.render('reservation.ejs')
  // MAIN POST ROUTE
+ 
 
  app.listen(3000, function(){ console.log('Breast Is Best app listening on port 3000'); });
+
+ 
+function bibMap() {
+  var btn = document.createElement("BUTTON");// Create a <button> element
+  btn.innerHTML = "CLICK ME";                   // Insert text
+  document.body.appendChild(btn);               // Append <button> to <body>
+}
