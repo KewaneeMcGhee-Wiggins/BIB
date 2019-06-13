186672aff789a821a323300b619d00a67eca2b0f
KewaneeMcGhee-Wiggins <47463588+KewaneeMcGhee-Wiggins@users.noreply.github.com>
2019-06-13 03:09:52 -0400

    added jpg to html, border and color to title, linked css pages properly, and made all 5 html pgs direct to each other in links and will make a nav bar

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-const express = require('express');
+const express = require('express');//modules
 const app = express();
 
-app.get('/', (req, res) => res.render('index.ejs'));//Homepage route
+app.get('/', (req, res) => res.render('index'));//Homepage route
 // or (req, res) => res.render('reservation.ejs')
  // MAIN POST ROUTE
  
-
+app.post('/')
  app.listen(3000, function(){ console.log('Breast Is Best app listening on port 3000'); });
-
+//listener
  
 function bibMap() {
   var btn = document.createElement("BUTTON");// Create a <button> element
